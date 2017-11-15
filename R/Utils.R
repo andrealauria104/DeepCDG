83a776e79d0a693c5fa1d2c02b42eb41fc0fff63
Andrea Lauria <andrea.lauria104@gmail.com>
2017-11-15 10:49:10 +0100

    mod

--- R/Utils.R
+++ R/Utils.R
@@ -40,24 +40,25 @@ store <- function(format, dir, data, cancer_type, data_category) {
   both = F
   if(format=="both"){
     both=T
+    format="Rdata"
   }
   i=1
   j=0
   while(i==1 & j<2){
     i=0
-    if(format=="Rdata" | both==T){
+    if(format=="Rdata"){
       # 1.3 Save Rdata
       Rdata_dir = paste0(dir,"/Rdata")
       dir.create(Rdata_dir, recursive = T)
       
-      cat("Saving expression data as .Rdata \n")
+      cat(paste0("Saving ", data_category," as .Rdata \n"))
       save(data, file = paste0(Rdata_dir,"/",cancer_type,"_", data_category, ".Rdata"))
     } else if(format=="csv"){
       # 1.3 Save CSV
       Rdata_dir = paste0(dir,"/CSV")
       dir.create(Rdata_dir, recursive = T)
       
-      cat("Saving expression data as .csv \n")
+      cat(paste0("Saving ", data_category," as .csv \n"))
       write.csv2(data, file = paste0(Rdata_dir,"/",cancer_type,"_", data_category, ".csv"))
     } else{
       stop(message("Invalid format for storing prepared data."))
